5a063e5283907d5be375b60c6bb9f64003cdee79
Miranda <88161424+MirandaT77@users.noreply.github.com>
2021-10-14 19:00:07 -0700

    finished markdown questions and formatting

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,133 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
 
+const generateMarkDown = require('./utils/generateMarkDown');
+
+//Prompt User
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser =  [
+        {
+            type: 'input',
+            name: 'Name',
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+              if (nameInput) {
+                return true;
+              } else {
+                console.log('Please enter your name!');
+                return false;
+              }
+            }
+          },
+        {
+          type: 'input',
+          name: 'Title',
+          message: 'What is the title of your project? (Required)',
+          validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please enter your project name!');
+              return false;
+            }
+          }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmDescription',
+            message: 'Would you like add a description about your project?',
+            default: true
+          },
+        {
+          type: 'input',
+          name: 'Description',
+          message: 'Please provide description of project:',
+          when: ({ confirmDescription }) => {
+            if (confirmDescription) {
+              return true;
+            } else {
+              return false;
+            }
+          }
+        },
+        {
+          type: 'input',
+          name: 'Contents',
+          message: 'Provide a table of contents to make it easy for users to find what they need:'
+        },
+        {
+          type: 'input',
+          name: 'Installation',
+          message: 'What are the steps required to install your project?'
+        },
+        {
+          type: 'input',
+          name: 'Usage',
+          message: 'Provide instructions and examples for use:'
+        },
+        {
+          type: 'checkbox',
+          name: 'License',
+          message: 'What License do you need for this project?',
+          choices: ['None', 'MIT', 'GNU GPLv3', 'APACHE', 'BSD3','ISC']
+        },
+        {
+          type: 'input',
+          name: 'Contributors',
+          message: 'Who are the contributors of this project?'
+        },
+        {
+          type: 'input',
+          name: 'Tests',
+          message: 'Write tests for your application and provide examples on how to run them:'
+        },
+        {
+          type: 'input',
+          name: 'Email',
+          message:'What is your email?',
+        },
+        {
+          type: 'input',
+          name: 'Github',
+          message: 'What is your GitHub username?'
+        },
+        {
+         type: 'input',
+         name: 'Questions',
+         message: 'How can you be reached for questions?'
+        },
+        {
+         type: 'confirm',
+         name: 'feature',
+         message: 'Would you like to feature this project?',
+         default: false
+        },
+        {
+         type: 'confirm',
+         name: 'confirmAddProject',
+         message: 'Would you like to enter another project?',
+         default: false
+          }
+        ];
+      
+      // promptUser()
+      // .then(answers => console.log(answers))
+      // .then(promptProject)
+      // .then(projectAnswers => console.log(projectAnswers))
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+  function writeToFile(fileName, data) {
+  return fs.writeFileSync (path.join(process.cwd(),fileName),data)
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+  function init() {
+  inquirer.prompt(promptUser).then((inquirerResponses=>{
+    writeToFile("readme.md",generateMarkDown({...inquirerResponses}))
+  }))
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,63 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(License) {
+  if (License !== 'None') {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  let badge = '';
+  if (License = 'MIT') {
+  return badge= '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+  }
+  else if (License = 'GNU GPLv3') {
+    return badge= '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+  }
+  else if (License = 'APACHE') {
+    return  badge= '![License: APACHE 2.0](https://img.shields.io/badge/License-APACHE2.0-blue.svg)';
+  }
+  else if (License = 'BSD3') {
+      return badge= '![License: BSD3](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+  }
+  else if (License = 'ISC') {
+      return badge= '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
+   }
+   
+  return '';
+ 
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
+// function renderLicenseLink(license) {}
+//   if (license) {
+//     return ''
+//   }
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
-
+ 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
 
+  # Title of README: ${data.Title}
+  # Name: ${data.Name} 
+
+  ## Table of Contents: ${data.Contents}
+  ## Description of Project: ${data.Description}
+  ## Installation Instructions: ${data.Installation}
+  ## Usage: ${data.Usage}
+  ## License: ${data.License}
+  ## Contributors: ${data.Title} was proudly made by ${data.Contributors}
+  ## Tests: ${data.Tests}
+
+
+  ## Questions: 
+  For any questions or feedback, please email me at: 
+  * ${data.Email} 
+  Check out my Github at: 
+  * (https://github.com/${data.Github})
+
+                
 `;
 }
 
